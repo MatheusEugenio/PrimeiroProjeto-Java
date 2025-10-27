05b886e81d864c130268467e9eb7a8dbeca654e5
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2025-10-26 23:04:53 -0300

    Ajustei algumas coisas na classe de Execução
    Tenho que rever a implementação dos métodos e a interação dos métodos com a entrada de dados

--- GerenciadorDeTarefas2.0/src/Exec.java
+++ GerenciadorDeTarefas2.0/src/Exec.java
@@ -1,14 +1,24 @@
 import GerenciadorDetasks.AplicacaoDasAcoes;
+import GerenciadorDetasks.Tarefa;
 
 import java.util.Scanner;
 
 public class Exec {
     public static void main(String[] args) {
+        Tarefa lavarcarro = new Tarefa("lavar o carro hoje", 1234567, "média");
 
         try (Scanner sc = new Scanner(System.in)){
             System.out.print("Digite o local/diretório do arquivo do gerenciador: ");
             String caminhoDefinitivo = sc.nextLine();
             AplicacaoDasAcoes gerenciador = new AplicacaoDasAcoes(caminhoDefinitivo);
+
+            gerenciador.listarTarefas();
+            gerenciador.addTarefa(sc); //Mudar esse parâmetro de scanner, tem que ser uma Tarefa
+            gerenciador.addTarefa(sc); //Mudar esse parâmetro de scanner, tem que ser uma Tarefa
+            gerenciador.listarTarefas();
+            gerenciador.marcarConcluido(lavarcarro, 1234);
+            gerenciador.removerTarefas(12345);
+
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
